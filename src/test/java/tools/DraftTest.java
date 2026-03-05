040d96f6e6750ba45971775a377486249be5e1e0
gala-zolo-forgood <galina.zolotareva@forgood.ai>
2026-03-05 09:19:59 +0200

    draft test

---
+++ src/test/java/tools/DraftTest.java
@@ -0,0 +1,18 @@
+package tools;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DraftTest {
+
+    @BeforeEach
+    public void setUp() {
+    }
+
+    @Test
+    public void draftTest() {
+        assertEquals(1, 1);
+    }
+}
